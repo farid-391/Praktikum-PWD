c1765d44537cdfbc4d6dbd605ca4b129e4ce8da0
Farid Afgar <faridafgar@gmail.com>
2021-12-03 23:25:30 +0700

    Second Update Post 7

--- searching/cari_khs.php
+++ searching/cari_khs.php
@@ -17,16 +17,22 @@ echo "<b>Hasil pencarian : ".$cari."</b>";
 <tr>
 <th>No</th>
 <th>NIM</th>
+<th>Nama</th>
 <th>Kode MK</th>
+<th>Nama MK</th>
 <th>Nilai</th>
 </tr>
 <?php 
 if(isset($_GET['cari'])){
 $cari = $_GET['cari'];
-$sql="select * from khs where nim like'%".$cari."%'";
+$sql="SELECT mahasiswa.nim, mahasiswa.nama, matakuliah.kode, matakuliah.nama as namamk, khs.nilai FROM khs 
+LEFT JOIN mahasiswa ON khs.nim = mahasiswa.nim 
+LEFT JOIN matakuliah ON khs.kodeMK = matakuliah.kode where mahasiswa.nim like'%".$cari."%'";
 $tampil = mysqli_query($con,$sql);
 }else{
-$sql="select * from khs";
+$sql="SELECT mahasiswa.nim, mahasiswa.nama, matakuliah.kode, matakuliah.nama as namamk, khs.nilai FROM khs 
+LEFT JOIN mahasiswa ON khs.nim = mahasiswa.nim 
+LEFT JOIN matakuliah ON khs.kodeMK = matakuliah.kode";
 $tampil = mysqli_query($con,$sql);
 }
 $no = 1;
@@ -35,7 +41,9 @@ while($r = mysqli_fetch_array($tampil)){
 <tr>
 <td><?php echo $no++; ?></td>
 <td><?php echo $r['nim']; ?></td>
-<td><?php echo $r['kodeMK']; ?></td>
+<td><?php echo $r['nama']; ?></td>
+<td><?php echo $r['kode']; ?></td>
+<td><?php echo $r['namamk']; ?></td>
 <td><?php echo $r['nilai']; ?></td>
 </tr>
 <?php } ?>
